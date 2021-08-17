0e122bdbfd0707d3a8ddfbc860357e241693802b
Hien Nguyen <FyreSchetz@yahoo.com>
2021-08-17 12:42:09 -0500

    add fight loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // Ask players if they want to fight or skip
@@ -54,6 +55,42 @@ var fight = function(enemyName) {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
 
+    // repeat and execute as long as the enemy-robot is alive 
+    while(enemyHealth > 0) {
+    // place fight function code block here . . .
+
+        // Ask players if they want to fight or skip
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        
+        // if player choses to fight, then fight
+        if (promptFight === "fight" || promptFight === "FIGHT") {
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+    
+        // check enemy's health
+        if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+        } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+    
+        // remove player's health by subtracting the amount set in the enemyAttack variable
+        playerHealth = playerHealth - enemyAttack;
+        console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+    
+        // check player's health
+        if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+        } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+        }
+    }
     // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
@@ -76,7 +113,8 @@ var fight = function(enemyName) {
 };
 
 //fight(enemyRobot);
-
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
